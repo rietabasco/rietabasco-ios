2f51c953033d753be385d2ddfd93f31101e354c6
Abiu Roldan <abiuroldan@Abius-MacBook-Pro.local>
2017-11-25 14:26:46 -0600

    first

---
+++ rietabasco-ios/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  rietabasco-ios
+//
+//  Created by Abiu Roldan on 11/25/17.
+//  Copyright © 2017 rietabasco. All rights reserved.
+//
+// Aplicación Oficial @rietabasco 
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
